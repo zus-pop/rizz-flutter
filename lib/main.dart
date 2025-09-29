b76faa9fd61a5553e25b395bf87c1727fe72dbb9
zus-pop <hoanganhtu3979@gmail.com>
2025-09-30 00:20:48 +0700

    update for ch play test

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:provider/provider.dart';
 import 'package:rizz_mobile/firebase_options.dart';
 import 'package:rizz_mobile/pages/bottom_tab_page.dart';
 import 'package:rizz_mobile/pages/details/detail_chat.dart';
-import 'package:rizz_mobile/pages/onboarding_screen.dart';
-import 'package:rizz_mobile/pages/profile_setup_page.dart';
 import 'package:rizz_mobile/pages/splash_screen.dart';
 import 'package:rizz_mobile/providers/app_setting_provider.dart';
 import 'package:rizz_mobile/providers/authentication_provider.dart';
@@ -48,7 +46,7 @@ class MyApp extends StatelessWidget {
         showPerformanceOverlay: false,
         title: "Rizz",
         theme: Provider.of<AppSettingProvider>(context).themeData,
-        home: const OnboardingScreen(),
+        home: const SplashScreen(),
         routes: {
           '/home': (context) => BottomTabPage(),
           '/detail_chat': (context) => const DetailChat(),
