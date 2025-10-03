7531b24769714d1aef598d353e2c08be03e450c3
zus-pop <hoanganhtu3979@gmail.com>
2025-10-04 01:16:07 +0700

    add file storage

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseService {
   static final FirebaseService _instance = FirebaseService._internal();
   final _firebaseMessaging = FirebaseMessaging.instance;
+  final _storage = FirebaseStorage.instance;
 
   factory FirebaseService() {
     return _instance;
@@ -18,5 +22,18 @@ class FirebaseService {
     debugPrint('The device token: [$token]');
 
     return token;
+  }
+
+  Future<String?> uploadFile(File file, String fileName) async {
+    try {
+      final storageRef = _storage.ref();
+      final fileRef = storageRef.child(fileName);
+      await fileRef.putFile(file);
+      final url = await fileRef.getDownloadURL();
+      return url;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
   }
 }
